c2047e00001c6b949660f92f894436b8e603f6d4
hello-dino <waahhh@naver.com>
2021-08-17 20:21:21 +0900

    Test Querydsl QClass And Lombok

--- src/test/java/co/kr/waahhh/querydsl/QuerydslApplicationTests.java
+++ src/test/java/co/kr/waahhh/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,35 @@
 package co.kr.waahhh.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Transactional
 class QuerydslApplicationTests {
+
+	@Autowired
+	EntityManager em;
 
 	@Test
 	void contextLoads() {
+		Dino dino = new Dino();
+		em.persist(dino);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+		QDino qDino = QDino.dino;
+
+		Dino result = query
+				.selectFrom(qDino)
+				.fetchOne();
+
+		assertThat(result).isEqualTo(dino);
+		assertThat(result.getId()).isEqualTo(dino.getId());
 	}
 
 }
